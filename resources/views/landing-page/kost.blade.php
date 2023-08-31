c32a79a3b3b1e064d6cc3b4df80ea7c07f3d7895
ihwan28 <ihwansyahcmc@gmail.com>
2023-08-31 11:22:26 +0700

    kos

--- resources/views/landing-page/kost.blade.php
+++ resources/views/landing-page/kost.blade.php
@@ -21,7 +21,6 @@
           <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                {{-- <a class="nav-link active" aria-current="page" href="#">SEVENKOS</a> --}}
               </li>
             </ul>
             <span class="navbar-text">
